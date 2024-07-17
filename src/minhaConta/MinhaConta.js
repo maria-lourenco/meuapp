1ec6407d45997e9c9879ac2c5747d162f1d92234
lucas2651 <134lucasalves@gmail.com>
2024-07-17 15:36:59 -0300

    modifiquei a pagina Minha conta

--- src/minhaConta/MinhaConta.js
+++ src/minhaConta/MinhaConta.js
@@ -1,6 +1,23 @@
-function MinhaConta(){
-    return(
-        <h1>Minha Conta</h1>
+import Cabecalho from "./cabeçalho/cabeçalho"
+import "./MinhaConta.css"
+function MinhaConta() {
+    return (
+        <div>
+            <Cabecalho />
+
+            <div class="card">
+                <h5 class="card-header">Informações pessoais</h5>
+               <div class="card-body">
+               <img className="imagenDoCliente" src="https://play-lh.googleusercontent.com/VCTiwwUfLPRh-8vrDraHfgVZOYaO8ArCVXkAKF8uv61Q7jTTVW2ox3jNjEEpgylzHsA" style={{width:90}}/>
+                    <div className="dadosPessoais">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     )
 }
 export default MinhaConta
---
+++ src/minhaConta/cabeçalho/cabeçalho.js
@@ -0,0 +1,14 @@
+import "./cabeçalho.css"
+
+function Cabecalho(){
+    return(
+        <div className="cabecalho">
+            <button className="btn btn-primary">munu</button>
+            <h1 className="titulo">Privacy settings</h1>
+            <p>Personal account</p>
+            <hr/>
+        </div>
+    )
+}
+
+export default Cabecalho
